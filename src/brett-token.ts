ec76ce3823f20c6ecf44750f2618005fda37f676
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2024-11-16 23:18:49 -0500

    4

--- src/brett-token.ts
+++ src/brett-token.ts
@@ -4,6 +4,7 @@ import {
   ExcludeFromLimits as ExcludeFromLimitsEvent,
   OwnershipTransferred as OwnershipTransferredEvent,
   SetAutomatedMarketMakerPair as SetAutomatedMarketMakerPairEvent,
+  SetBuyFees as SetBuyFeesEvent,
   SwapAndLiquify as SwapAndLiquifyEvent,
   TokensAirdropped as TokensAirdroppedEvent,
   Transfer as TransferEvent,
@@ -29,7 +30,7 @@ import {
   GlobalSettings,
 } from "../generated/schema";
 
-import { BigInt, Bytes, ethereum } from "@graphprotocol/graph-ts";
+import { BigInt, Bytes } from "@graphprotocol/graph-ts";
 
 /** Helper Functions **/
 
@@ -50,9 +51,9 @@ function getTokenAnalytics(): TokenAnalytics {
 }
 
 function getHolder(address: Bytes): Holder {
-  let holder = Holder.load(address.toHex());
+  let holder = Holder.load(address);
   if (!holder) {
-    holder = new Holder(address.toHex());
+    holder = new Holder(address);
     holder.balance = BigInt.zero();
     holder.totalReceived = BigInt.zero();
     holder.totalSent = BigInt.zero();
@@ -118,24 +119,143 @@ export function handleTransfer(event: TransferEvent): void {
   receiver.transactionCount += 1;
   receiver.save();
 
-  if (!Holder.load(event.params.to.toHex())) {
+  if (!Holder.load(event.params.to)) {
     analytics.uniqueHolders += 1;
     analytics.save();
   }
 }
 
-/** Call Handlers **/
-
-export function handleBuyFees(call: ethereum.Call): void {
+export function handleSetBuyFees(event: SetBuyFeesEvent): void {
   let settings = getGlobalSettings();
-  let buyFeesResult = call.outputs[0].toBigInt();
-  settings.buyFees = buyFeesResult;
+  settings.buyFees = event.params.buyFees;
+  settings.blockTimestamp = event.block.timestamp;
   settings.save();
 }
 
-export function handleSellFees(call: ethereum.Call): void {
+export function handleExcludeFromFees(event: ExcludeFromFeesEvent): void {
+  let entity = new ExcludeFromFees(event.transaction.hash.concatI32(event.logIndex.toI32()));
+  entity.account = event.params.account;
+  entity.isExcluded = event.params.isExcluded;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+  entity.save();
+}
+
+export function handleExcludeFromLimits(event: ExcludeFromLimitsEvent): void {
+  let entity = new ExcludeFromLimits(event.transaction.hash.concatI32(event.logIndex.toI32()));
+  entity.account = event.params.account;
+  entity.isExcluded = event.params.isExcluded;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+  entity.save();
+}
+
+export function handleOwnershipTransferred(event: OwnershipTransferredEvent): void {
+  let entity = new OwnershipTransferred(event.transaction.hash.concatI32(event.logIndex.toI32()));
+  entity.previousOwner = event.params.previousOwner;
+  entity.newOwner = event.params.newOwner;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+  entity.save();
+
   let settings = getGlobalSettings();
-  let sellFeesResult = call.outputs[0].toBigInt();
-  settings.sellFees = sellFeesResult;
+  settings.tradingActive = true;
+  settings.blockTimestamp = event.block.timestamp;
+  settings.save();
+}
+
+export function handleSetAutomatedMarketMakerPair(event: SetAutomatedMarketMakerPairEvent): void {
+  let entity = new SetAutomatedMarketMakerPair(event.transaction.hash.concatI32(event.logIndex.toI32()));
+  entity.pair = event.params.pair;
+  entity.value = event.params.value;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+  entity.save();
+}
+
+export function handleSwapAndLiquify(event: SwapAndLiquifyEvent): void {
+  let entity = new SwapAndLiquify(event.transaction.hash.concatI32(event.logIndex.toI32()));
+  entity.tokensSwapped = event.params.tokensSwapped;
+  entity.ethReceived = event.params.ethReceived;
+  entity.tokensIntoLiquidity = event.params.tokensIntoLiquidity;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+  entity.save();
+
+  let analytics = getTokenAnalytics();
+  analytics.totalLiquidityAdded = analytics.totalLiquidityAdded.plus(event.params.tokensIntoLiquidity);
+  analytics.blockTimestamp = event.block.timestamp;
+  analytics.save();
+}
+
+export function handleTokensAirdropped(event: TokensAirdroppedEvent): void {
+  let entity = new TokensAirdropped(event.transaction.hash.concatI32(event.logIndex.toI32()));
+  entity.totalWallets = event.params.totalWallets;
+  entity.totalTokens = event.params.totalTokens;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+  entity.save();
+
+  let analytics = getTokenAnalytics();
+  analytics.totalAirdropped = analytics.totalAirdropped.plus(event.params.totalTokens);
+  analytics.blockTimestamp = event.block.timestamp;
+  analytics.save();
+}
+
+export function handledevelopmentWalletUpdated(event: developmentWalletUpdatedEvent): void {
+  let entity = new developmentWalletUpdated(event.transaction.hash.concatI32(event.logIndex.toI32()));
+  entity.newWallet = event.params.newWallet;
+  entity.oldWallet = event.params.oldWallet;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+  entity.save();
+}
+
+export function handleliquidityWalletUpdated(event: liquidityWalletUpdatedEvent): void {
+  let entity = new liquidityWalletUpdated(event.transaction.hash.concatI32(event.logIndex.toI32()));
+  entity.newWallet = event.params.newWallet;
+  entity.oldWallet = event.params.oldWallet;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+  entity.save();
+}
+
+export function handlemarketingWalletUpdated(event: marketingWalletUpdatedEvent): void {
+  let entity = new marketingWalletUpdated(event.transaction.hash.concatI32(event.logIndex.toI32()));
+  entity.newWallet = event.params.newWallet;
+  entity.oldWallet = event.params.oldWallet;
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+  entity.save();
+}
+
+// New handlers for analytics and settings
+export function handleTransferForAnalytics(event: TransferEvent): void {
+  let analytics = getTokenAnalytics();
+  analytics.totalTransfers = analytics.totalTransfers.plus(BigInt.fromI32(1));
+  analytics.blockTimestamp = event.block.timestamp;
+  analytics.save();
+}
+
+export function handleTokensAirdroppedForAnalytics(event: TokensAirdroppedEvent): void {
+  let analytics = getTokenAnalytics();
+  analytics.totalAirdropped = analytics.totalAirdropped.plus(event.params.totalTokens);
+  analytics.blockTimestamp = event.block.timestamp;
+  analytics.save();
+}
+
+export function handleOwnershipTransferredForSettings(event: OwnershipTransferredEvent): void {
+  let settings = getGlobalSettings();
+  settings.tradingActive = true; // Example update
+  settings.blockTimestamp = event.block.timestamp;
   settings.save();
 }
